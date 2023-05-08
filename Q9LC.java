7e647fc3b1abee1243c128aee06003ecaa543230
Harsh Sahu <harshsahu7765858311@gmail.com>
2023-05-08 20:51:05 +0530

    Changes to be committed: 	modified:   Q9LC.java

--- Q9LC.java
+++ Q9LC.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         int rev=0;int num=x;
         
         if(x<0){
             return false;
         }
         else{
-            while(num>=0){
+            while(num>0){
             rev = rev*10 + num%10;
             num = num/10;
 
@@ -19,5 +19,8 @@ class Solution {
             }
         }
         
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
     }
 }
