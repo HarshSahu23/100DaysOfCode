64bfba751946133aa2c43e9762b0b912ac4ef76d
Harsh Sahu <harshsahu7765858311@gmail.com>
2023-05-09 17:18:59 +0530

    Changes to be committed: 	modified:   Q7LC.java 	modified:   test.java

--- Q7LC.java
+++ Q7LC.java
@@ -1,39 +1,44 @@
-class Q7LC {
+public class Q7LC
+{
     public static int reverse(int x) {
-        int num=x;String rev="";int r=0;
+        int r=0;int num;String store="";
+        String maxInt = Integer.toString(Integer.MAX_VALUE);
+        String minInt = "2147483648";
+
         if (x<0) {
             num=0-x;
-            while(num>0){
-                rev+=Integer.toString(num%10);
-                num=num/10;
+            while (num>0) {
+                store+=Integer.toString(num%10);
+                num/=10;
             }
-            if ((rev.compareTo("2147483648")>0)&&(rev.length()==9)) {
+            if ((store.compareTo(minInt)>0)&&(store.length()>=minInt.length())) {
                 r=0;
             }else{
-                r=0-Integer.parseInt(rev);
+                if (store!="") {
+                    r=0-Integer.parseInt(store);
+                }
             }
-            System.out.println(rev);
-        }
-        if(x>0){
-            while(num>0);{
-                rev+=Integer.toString(num%10);
-                num=num/10;
+        } else {
+            num=x;
+            while (num>0) {
+                store+=Integer.toString(num%10);
+                num/=10;
             }
-            System.out.println(rev);
-            if ((rev.compareTo("2147483647")>0)&&(rev.length()==9)) {
+            
+            if ((store.compareTo(maxInt)>0)&&(store.length()>=maxInt.length())) {
                 r=0;
             }else{
-                r=Integer.parseInt(rev);
+                if (store!="") {
+                    r=Integer.parseInt(store);
+                }
+                
             }
-            System.out.println(rev);
+            
         }
-        if (x==0) {
-            r=0;
-        }
-        System.out.println(rev);
         return r;
     }
-    public static void main(String[] args) {
-        System.out.println(reverse(1534236469));
+    public static void main(String[] args)
+    {
+        System.out.println("ans = "+reverse(-2147483648));
     }
 }
